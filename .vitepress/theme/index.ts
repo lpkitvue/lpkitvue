d7906c39a176034e7eb26037e0a7c6cd388630d0
burakergen <burak.ergen@outlook.com.tr>
2025-05-14 22:16:11 +0300

    Remove unused HTML transformation for home layout in config

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,6 +1,6 @@
 import { h, onMounted } from 'vue'
 import DefaultTheme from 'vitepress/theme'
-import { setupGoogleAnalytics } from './analytics'
+// import { setupGoogleAnalytics } from './analytics'
 import './style.css'
 
 const GA_MEASUREMENT_ID = 'G-TFVL7Y9FEK'
@@ -14,17 +14,17 @@ export default {
   },
   enhanceApp({ app, router, siteData }) {
     onMounted(() => {
-      setupGoogleAnalytics(GA_MEASUREMENT_ID)
-      console.log('App mounted')
-
-      router.onAfterRouteChanged = (to) => {
-        if (window.gtag) {
-          window.gtag('event', 'page_view', {
-            page_path: to,
-            page_title: document.title
-          })
-        }
-      }
+      // setupGoogleAnalytics(GA_MEASUREMENT_ID)
+      // console.log('App mounted')
+      //
+      // router.onAfterRouteChanged = (to) => {
+      //   if (window.gtag) {
+      //     window.gtag('event', 'page_view', {
+      //       page_path: to,
+      //       page_title: document.title
+      //     })
+      //   }
+      // }
     })
   }
 }
